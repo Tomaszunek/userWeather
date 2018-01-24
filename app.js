ab09c895207d5b7a0e7e8312cbcad883c5f9fc8f
Tomasz <pawlowskitomaszuu@gmail.com>
2018-01-24 19:59:17 +0100

    24.1 # adding api to psyche

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const url = require('url');
 const http = require('http');
-const psyche = require('./routers/psycheAPI.js')
+const psychename = require('./routers/psyche/testName.js')
 
 //Models
 var models = require('./models');
@@ -19,10 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const router = express.Router();
 
-app.use('/psyche', psyche);
+app.use('/psyche/testname', psychename);
 
 //Sync Database {force:true}
-models.sequelize.sync({force:true}).then(function(){
+models.sequelize.sync().then(function(){
   console.log('Nice! Database looks fine');
 }).catch(function(err){
   console.log(err,"Something went wrong with the Database Update!");
---
+++ routers/psyche/typeDesc.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const models = require('../models');
+const Sequelize = require('sequelize');
+
+const router = express.Router();
+
+
+
+module.exports = router;
