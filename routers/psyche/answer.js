3ff7c5b73f6847c8d7b1ea9ce041b70f810e4eec
Tomasz <pawlowskitomaszuu@gmail.com>
2018-01-27 21:31:41 +0100

    27.1 # answer API CRUD

--- routers/psyche/answer.js
+++ routers/psyche/answer.js
@@ -17,5 +17,47 @@ router.get('/getAnswers',function(req, res){
   });
 });
 
+router.get('/getAnswer/:id',function(req, res){
+  models.PsycheAnswer.findOne({where: {'id' : req.params.id}}).then((psycheAnswer) => {
+    res.send(psycheAnswer);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.post('/addQuestion/:questionId',function(req, res){
+  let answer = {
+    'questionId' : req.params.questionId,
+    'answer' : req.body.answer,
+    'answerValue' : req.body.answerValue
+  }
+  models.PsycheAnswer.create(answer).then((psycheAnswer) => {
+    res.send(psycheAnswer);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.put('/updateQuestion/:id',function(req, res){
+  let answer = {
+    'questionId' : req.body.questionId,
+    'answer' : req.body.answer,
+    'answerValue' : req.body.answerValue
+  }
+  models.PsycheAnswer.update(answer, {where: {'id' : req.params.id}}).then((psycheAnswer) => {
+    res.send(psycheAnswer);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.delete('/deleteQuestion/:id',function(req, res){
+  models.PsycheAnswer.destroy({where: {'id' : req.params.id}}).then(() => {
+    res.send();
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
 
 module.exports = router;
