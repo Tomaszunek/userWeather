26fcc5adc747f5c5263124889b964aa7a2597926
Tomasz <pawlowskitomaszuu@gmail.com>
2018-01-27 21:17:48 +0100

    27.1 # questionScore API CRUD

--- routers/psyche/questionScore.js
+++ routers/psyche/questionScore.js
@@ -16,4 +16,52 @@ router.get('/getQuestionsScore',function(req, res){
   });
 });
 
+router.get('/getQuestionScore/:id',function(req, res){
+  models.PsycheQuestionScore.findOne({where: {'id' : req.params.id}}).then((questionScore) => {
+    res.send(questionScore);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.post('/addQuestionScore/:questionId',function(req, res){
+  let questionScore = {
+    'questionId' : req.params.questionId,
+    'mind' : req.body.mind,
+    'energy' : req.body.energy,
+    'nature' : req.body.nature,
+    'tactict' : req.body.tactict,
+    'indentity' : req.body.indentity
+  }
+  models.PsycheQuestionScore.create(questionScore).then((questionScore) => {
+    res.send(questionScore);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.put('/updateQuestionScore/:questionId',function(req, res){
+  let questionScore = {
+    'questionId' : req.params.questionId,
+    'mind' : req.body.mind,
+    'energy' : req.body.energy,
+    'nature' : req.body.nature,
+    'tactict' : req.body.tactict,
+    'indentity' : req.body.indentity
+  }
+  models.PsycheQuestionScore.update(questionScore, {where: {'questionId' : req.params.questionId}}).then((questionScore) => {
+    res.send(questionScore);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.delete('/deleteQuestionScore/:questionId',function(req, res){
+  models.PsycheQuestionScore.destroy({where: {'questionId' : req.params.questionId}}).then(() => {
+    res.send();
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
 module.exports = router;
