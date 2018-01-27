7f23220dcf29da033371820d55d46713a53bf9f5
Tomasz <pawlowskitomaszuu@gmail.com>
2018-01-27 21:22:48 +0100

    27.1 # question API CRUD

--- routers/psyche/question.js
+++ routers/psyche/question.js
@@ -26,4 +26,50 @@ router.get('/getQuestions/:testId',function(req, res){
   });
 });
 
+router.get('/getQuestions/:id',function(req, res){
+  models.PsycheQuestion.findOne({where: {'id' : req.params.id}}).then((psycheQuestion) => {
+    res.send(psycheQuestion);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.post('/addQuestion/:testId',function(req, res){
+  let question = {
+    'testId' : req.params.testId,
+    'order' : req.body.order,
+    'questionName' : req.body.questionName,
+    'questionBody' : req.body.questionBody
+  }
+  models.PsycheQuestion.create(question).then((question) => {
+    res.send(question);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.put('/updateQuestionScore/:id',function(req, res){
+  let question = {
+    'testId' : req.body.testId,
+    'order' : req.body.order,
+    'questionName' : req.body.questionName,
+    'questionBody' : req.body.questionBody
+  }
+  models.PsycheQuestion.update(question, {where: {'id' : req.params.id}}).then((question) => {
+    res.send(questionScore);
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.delete('/deleteQuestionScore/:id',function(req, res){
+  models.PsycheQuestionScore.destroy({where: {'id' : req.params.id}}).then(() => {
+    res.send();
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+
+
 module.exports = router;
