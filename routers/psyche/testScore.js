d1b04d7fd1351d1f1b7f9913153e172aa847464a
Tomasz <pawlowskitomaszuu@gmail.com>
2018-01-27 20:41:37 +0100

    27.1 # testScore API CRUD

--- routers/psyche/testScore.js
+++ routers/psyche/testScore.js
@@ -16,4 +16,47 @@ router.get('/getTestScores',function(req, res){
   });
 });
 
+router.get('/getTestScore/:id',function(req, res){
+  models.PsycheTestScore.findOne({where: {'id' : req.params.id}}).then((psycheTestScore) => {
+    if(psycheTestScore){
+      res.send(psycheTestScore);
+    }
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.post('/createTestScore',function(req, res){
+  let psycheTestScore = {
+    'typeId' : req.body.typeId,
+    'mind' : req.body.mind,
+    'energy' : req.body.energy,
+    'nature' : req.body.nature,
+    'tactict' : req.body.tactict,
+    'indentity' : req.body.indentity
+  };
+  models.PsycheTestScore.create(psycheTestScore).then((psycheTestScore) => {
+    res.send(psycheTestScore);
+  });
+});
+
+router.put('/updateTestScore/:typeId',function(req, res){
+  models.PsycheTestScore.update({
+    'typeId' : req.body.typeId,
+    'mind' : req.body.mind,
+    'energy' : req.body.energy,
+    'nature' : req.body.nature,
+    'tactict' : req.body.tactict,
+    'indentity' : req.body.indentity
+  }, { where: {'testId' : req.params.typeId}}).then((psycheTestScore) => {
+    res.send(psycheTestScore);
+  });
+});
+
+router.delete('/deleteTestScore/:typeId',function(req, res){
+  models.PsycheTestScore.destroy({where: {'typeId' : req.params.typeId}}).then(() => {
+    res.send();
+  });
+});
+
 module.exports = router;
