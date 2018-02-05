91740a6feb20a9e4faab94d83bf61ffb44795b44
Tomasz <pawlowskitomaszuu@gmail.com>
2018-02-05 22:45:34 +0100

    5.2 add mailing, acctive acc,get user

--- routers/user/user.js
+++ routers/user/user.js
@@ -18,6 +18,18 @@ router.get('/getUsers',function(req, res){
   });
 });
 
+router.get('/getUser/:id',function(req, res){
+  models.User.findAll({where: {'id' : req.params.id},
+    include : { model: models.UserDetail, as : 'UserDetail'
+  }}).then((user) => {
+    if(user){
+      res.send(user);
+    }
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
 router.post('/registerUser',function(req, res){
   var salt = bcrypt.genSaltSync(10);
    var hash = bcrypt.hashSync(req.body.password, salt);
@@ -40,6 +52,39 @@ router.post('/registerUser',function(req, res){
     res.send(err);
   });
 });
+
+router.post('/createDetails/:id',function(req, res){
+   var userDetail = {
+      firstName  : req.body.firstName,
+      lastName   : req.body.lastName,
+      gender     : req.body.gender,
+      address    : req.body.address,
+      birthDay   : req.body.address,
+  };
+  models.UserDetail.find({where: {userId : req.params.id}}).then((user) => {
+    if(user){
+      res.send("Details for that user just exists");
+    } else {
+      models.UserDetail.create(userDetail).then(() =>{
+        res.sendStatus(200);
+      });
+    }
+  }).catch(function(err){
+    res.send(err);
+  });
+});
+
+router.get('/acceptMail/:username',function(req, res){
+  models.User.find({where: {username : req.params.username}}).then((user) => {
+    if(user){
+      models.User.update({'activeAcc' : true},{where : {username : req.params.username}}).then((user) => {
+        res.sendStatus(200);
+      });
+    }
+  });
+});
+
+
 
 router.post('/loginUser',function(req, res){
   models.User.find({where: {[Sequelize.Op.or] : [{'username' : req.body.username}, {'email' : req.body.username}]}}).then((user) => {
