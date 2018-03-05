126e64d44aa92b37c6dc867713ee24e17cc17355
Tomasz <pawlowskitomaszuu@gmail.com>
2018-03-05 19:52:43 +0100

    changing user managment

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
  
 import { User } from '../_models/index';
 import { UserService, WeatherService } from '../_services/index';
+import { Observable } from 'rxjs/Rx';
  
 @Component({
     moduleId: module.id,
@@ -11,14 +12,17 @@ import { UserService, WeatherService } from '../_services/index';
 export class HomeComponent implements OnInit {
     currentUser: any = {};
     users: User[] = [];
+    usersFound: boolean;
+    whetherFound: boolean;
     weather: any = {};
  
     constructor(private userService: UserService, private weatherService : WeatherService) {       
     }
  
     ngOnInit() {
+        this.usersFound = false;
+        this.whetherFound = false;
         this.loadAllUsers();
-
     }
 
     selectUser(id: number) {
@@ -28,7 +32,7 @@ export class HomeComponent implements OnInit {
            this.weatherService.getWeather(
              this.currentUser.UserCountry.sortname,
              this.currentUser.UserCity.cityName)
-             .subscribe((weather) => {this.weather = weather;});
+             .subscribe((weather) => {this.weather = weather; this.whetherFound = true;});
          });
     }
  
@@ -38,6 +42,8 @@ export class HomeComponent implements OnInit {
  
     private loadAllUsers() {
         this.userService.getAll().subscribe(users => { this.users = users;
-        this.selectUser(users[0].id))});
+        this.selectUser(users[0].id);
+        this.usersFound = true;
+      });
     }
-}
+  }
